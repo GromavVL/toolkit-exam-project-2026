237b310bb4e77f2842dcea7627b103375eb1df51
Vlad <vlad086367@gmail.com>
2026-07-02 17:41:36 +0300

    Refactor userRoutes.js

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,37 +1,17 @@
 const express = require('express');
 const basicMiddlewares = require('../middlewares/basicMiddlewares');
-const hashPass = require('../middlewares/hashPassMiddle');
-const userController = require('../controllers/userController');
 const contestController = require('../controllers/contestController');
 const checkToken = require('../middlewares/checkToken');
-const validators = require('../middlewares/validators');
 const chatController = require('../controllers/chatController');
 const upload = require('../utils/fileUpload');
 const router = express.Router();
 
-router.post(
-  '/registration',
-  validators.validateRegistrationData,
-  hashPass,
-  userController.registration
-);
-
-router.post('/login', validators.validateLogin, userController.login);
+const userRoutes = require('./userRoutes')
 
 router.post(
   '/dataForContest',
   checkToken.checkToken,
   contestController.dataForContest
-);
-
-router.post(
-  '/pay',
-  checkToken.checkToken,
-  basicMiddlewares.onlyForCustomer,
-  upload.uploadContestFiles,
-  basicMiddlewares.parseBody,
-  validators.validateContestCreation,
-  userController.payment
 );
 
 router.post(
@@ -84,27 +64,6 @@ router.post(
   contestController.setOfferStatus
 );
 
-router.post(
-  '/changeMark',
-  checkToken.checkToken,
-  basicMiddlewares.onlyForCustomer,
-  userController.changeMark
-);
-
-router.post(
-  '/updateUser',
-  checkToken.checkToken,
-  upload.uploadAvatar,
-  userController.updateUser
-);
-
-router.post(
-  '/cashout',
-  checkToken.checkToken,
-  basicMiddlewares.onlyForCreative,
-  userController.cashout
-);
-
 router.post('/newMessage', checkToken.checkToken, chatController.addMessage);
 
 router.post('/getChat', checkToken.checkToken, chatController.getChat);
@@ -146,5 +105,7 @@ router.post(
 );
 
 router.post('/getCatalogs', checkToken.checkToken, chatController.getCatalogs);
+
+router.use('/', userRoutes);
 
 module.exports = router;
---
+++ server/src/router/userRoutes.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const validators = require('../middlewares/validators');
+const hashPass = require('../middlewares/hashPassMiddle');
+const checkToken = require('../middlewares/checkToken');
+const basicMiddlewares = require('../middlewares/basicMiddlewares');
+const upload = require('../utils/fileUpload');
+
+const router = express.Router();
+
+router.post('/login', validators.validateLogin, userController.login);
+
+router.post(
+  '/registration',
+  validators.validateRegistrationData,
+  hashPass,
+  userController.registration
+);
+
+router.post(
+  '/pay',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCustomer,
+  upload.uploadContestFiles,
+  basicMiddlewares.parseBody,
+  validators.validateContestCreation,
+  userController.payment
+);
+
+router.post(
+  '/updateUser',
+  checkToken.checkToken,
+  upload.uploadAvatar,
+  userController.updateUser
+);
+
+router.post(
+  '/cashout',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCreative,
+  userController.cashout
+);
+
+router.post(
+  '/changeMark',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCustomer,
+  userController.changeMark
+);
+
+module.exports = router;
