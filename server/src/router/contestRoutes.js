83b1ed35dca4d005190e463702986ebf4ecda69e
Vlad <vlad086367@gmail.com>
2026-07-02 18:04:35 +0300

    Refactor contestRoutes

---
+++ server/src/router/contestRoutes.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const checkToken = require('../middlewares/checkToken');
+const contestController = require('../controllers/contestController');
+const basicMiddlewares = require('../middlewares/basicMiddlewares');
+const upload = require('../utils/fileUpload');
+
+const router = express.Router();
+
+router.post(
+  '/dataForContest',
+  checkToken.checkToken,
+  contestController.dataForContest
+);
+
+router.post(
+  '/getCustomersContests',
+  checkToken.checkToken,
+  contestController.getCustomersContests
+);
+
+router.get(
+  '/getContestById',
+  checkToken.checkToken,
+  basicMiddlewares.canGetContest,
+  contestController.getContestById
+);
+
+router.post(
+  '/getAllContests',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCreative,
+  contestController.getContests
+);
+
+router.get(
+  '/downloadFile/:fileName',
+  checkToken.checkToken,
+  contestController.downloadFile
+);
+
+router.post(
+  '/updateContest',
+  checkToken.checkToken,
+  upload.updateContestFile,
+  contestController.updateContest
+);
+
+router.post(
+  '/setNewOffer',
+  checkToken.checkToken,
+  upload.uploadLogoFiles,
+  basicMiddlewares.canSendOffer,
+  contestController.setNewOffer
+);
+
+router.post(
+  '/setOfferStatus',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCustomerWhoCreateContest,
+  contestController.setOfferStatus
+);
+
+module.exports = router;
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,68 +1,12 @@
 const express = require('express');
-const basicMiddlewares = require('../middlewares/basicMiddlewares');
-const contestController = require('../controllers/contestController');
 const checkToken = require('../middlewares/checkToken');
 const chatController = require('../controllers/chatController');
-const upload = require('../utils/fileUpload');
 const router = express.Router();
 
-const userRoutes = require('./userRoutes')
-
-router.post(
-  '/dataForContest',
-  checkToken.checkToken,
-  contestController.dataForContest
-);
-
-router.post(
-  '/getCustomersContests',
-  checkToken.checkToken,
-  contestController.getCustomersContests
-);
-
-router.get(
-  '/getContestById',
-  checkToken.checkToken,
-  basicMiddlewares.canGetContest,
-  contestController.getContestById
-);
-
-router.post(
-  '/getAllContests',
-  checkToken.checkToken,
-  basicMiddlewares.onlyForCreative,
-  contestController.getContests
-);
+const userRoutes = require('./userRoutes');
+const contestRoutes = require('./contestRoutes');
 
 router.post('/getUser', checkToken.checkAuth);
-
-router.get(
-  '/downloadFile/:fileName',
-  checkToken.checkToken,
-  contestController.downloadFile
-);
-
-router.post(
-  '/updateContest',
-  checkToken.checkToken,
-  upload.updateContestFile,
-  contestController.updateContest
-);
-
-router.post(
-  '/setNewOffer',
-  checkToken.checkToken,
-  upload.uploadLogoFiles,
-  basicMiddlewares.canSendOffer,
-  contestController.setNewOffer
-);
-
-router.post(
-  '/setOfferStatus',
-  checkToken.checkToken,
-  basicMiddlewares.onlyForCustomerWhoCreateContest,
-  contestController.setOfferStatus
-);
 
 router.post('/newMessage', checkToken.checkToken, chatController.addMessage);
 
@@ -107,5 +51,6 @@ router.post(
 router.post('/getCatalogs', checkToken.checkToken, chatController.getCatalogs);
 
 router.use('/', userRoutes);
+router.use('/', contestRoutes);
 
 module.exports = router;
