cb86ba5c6b675839626cd250f84b0ada2afcb91a
Vlad <vlad086367@gmail.com>
2026-07-05 15:28:20 +0300

    Fix style on the ChatInut

--- client/src/components/Chat/ChatComponents/ChatInut/ChatInput.jsx
+++ client/src/components/Chat/ChatComponents/ChatInut/ChatInput.jsx
@@ -33,6 +33,7 @@ const ChatInput = props => {
               container: styles.container,
               input: styles.input,
               notValid: styles.notValid,
+              warning: styles.fieldWarning,
             }}
           />
           <button type='submit'>
