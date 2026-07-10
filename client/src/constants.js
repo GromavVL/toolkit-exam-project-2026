ae642ac6b6b8ce3a21fe858ac7f876645fdc4079
Vlad <vlad086367@gmail.com>
2026-07-10 20:00:13 +0300

    Мove card content to constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -81,4 +81,21 @@ export default {
       items: ['Terms of Service', 'Privacy Policy', 'Cookie Policy'],
     },
   ],
+  Cards: [
+    {
+      img: 'card1.svg',
+      title: 'Launch a Contest',
+      body: 'Work with hundreds of creative experts to get custom name suggestions for your business or brand. All names are auto-checked for URL availability.',
+    },
+    {
+      img: 'card2.svg',
+      title: 'Explore Names For Sale',
+      body: 'Our branding team has curated thousands of pre-made names that you can purchase instantly. All names include a matching URL and a complimentary Logo Design',
+    },
+    {
+      img: 'card3.svg',
+      title: 'Agency-level Managed Contests',
+      body: 'Our Managed contests combine the power of crowdsourcing with the rich experience of our branding consultants. Get a complete agency-level experience at a fraction of Agency costs',
+    },
+  ],
 };
