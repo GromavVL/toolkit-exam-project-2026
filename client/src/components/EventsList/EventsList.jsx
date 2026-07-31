d9ef0b4b90430ec0c2d54c19b59ab1d581a2a788
Vlad <vlad086367@gmail.com>
2026-07-31 19:58:14 +0300

    Create structure event list

--- client/src/components/EventsList/EventsList.jsx
+++ client/src/components/EventsList/EventsList.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import styles from './EventsList.module.sass';
+import EventTimer from '../EventTimer/EventTimer';
+import { BsClockHistory } from 'react-icons/bs';
 
-function EventsList () {
-  return <div>EventsList</div>;
+function EventsList ({ events, setEvents }) {
+  const handleDelete = indexToDelete => {
+    setEvents(prevEvents => prevEvents.filter((el, i) => i !== indexToDelete));
+  };
+  return (
+    <>
+      <div className={styles.headingEventList}>
+        <h2 className={styles.titleContent}>Live upcomming checks </h2>
+        <div className={styles.timeBlock}>
+          <p className={styles.timeBlockContent}>Remaming time</p>
+          <BsClockHistory className={styles.titleIcon} />
+        </div>
+      </div>
+      <ul className={styles.timeRenderList}>
+        {events.length === 0 ? (
+          <li className={styles.massageNoEvents}>No events available</li>
+        ) : (
+          events.map((e, i) => (
+            <EventTimer event={e} key={i} onDelete={() => handleDelete(i)} />
+          ))
+        )}
+      </ul>
+    </>
+  );
 }
 
 export default EventsList;
