eac385747018210b67edce6fed033f4dc1c61b99
Vlad <vlad086367@gmail.com>
2026-09-19 20:00:51 +0300

    Connect moderator page to redux and style

--- client/src/pages/ModeratorPage/ModeratorPage.jsx
+++ client/src/pages/ModeratorPage/ModeratorPage.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import styles from './ModeratorPage.module.sass';
+import { getPendingOffers } from '../../store/slices/contestByIdSlice';
+import withRouter from '../../hocs/withRouter';
+import CONSTANTS from '../../constants';
+import OfferBoxReview from '../../components/OfferBox/OfferBoxReview/OfferBoxReview';
 
-function ModeratorPage () {
-  return <div>ModeratorPage</div>;
-}
+const ModeratorPage = props => {
+  const {
+    contestByIdStore: { offers },
+    getPendingOffers,
+  } = props;
+  useEffect(() => {
+    getPendingOffers();
+  }, []);
 
-export default ModeratorPage;
+  return (
+    <main className={styles.moderatorWrapper}>
+      <section className={styles.reviewOfferBox}>
+        <h2 className={styles.titleModerator}>
+          Offers to review: {offers.length}
+        </h2>
+        <button
+          onClick={() => props.getPendingOffers()}
+          className={styles.refreshButton}
+        >
+          Refresh
+        </button>
+        {props.userStore.data.role === CONSTANTS.MODERATOR ? (
+          <OfferBoxReview offers={offers} />
+        ) : (
+          <p>You are not a moderator</p>
+        )}
+      </section>
+    </main>
+  );
+};
+
+const mapStateToProps = state => {
+  const { contestByIdStore, userStore } = state;
+  return { contestByIdStore, userStore };
+};
+
+const mapDispatchToProps = dispatch => ({
+  getPendingOffers: () => dispatch(getPendingOffers()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(ModeratorPage));
